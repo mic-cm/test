a9a9d5b9b76e47d9cb39cc2761e539cd9ba2e9aa
CM <2238336938@qq.com>
2020-12-07 11:33:32 +0800

    完成了index页面

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,25 @@
+
 $.ajaxPrefilter(function (options) {
+    // 拼接根路径与请求目标路径，options指的是$.ajax里的值
     options.url = 'http://ajax.frontend.itheima.net' + options.url;
+
+    //提取了请求头部
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+    //提取了complete回调函数
+    //不管成功与否都调用回调函数
+    options.complete = function (res) {
+        // console.log(res);
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            //清空token
+            localStorage.removeItem('token');
+            // 2. 重新跳转到登录页面
+            location.href = '/login.html'
+
+        }
+    }
 })
-//$.ajaxPrefilter函数用于拼接根路径与请求目标路径，options指的是$.ajax里的值
---
+++ assets/js/index.js
@@ -0,0 +1,60 @@
+$(function () {
+    //调用用户信息
+    getuserinfo();
+
+
+    //注册退出事件
+    var layer = layui.layer;
+    $('#quit').on('click', function () {
+        layer.confirm('确定退出吗?', { icon: 3, title: '提示' }, function (index) {
+            //清空token
+            localStorage.removeItem('token');
+            // 2. 重新跳转到登录页面
+            location.href = '/login.html'
+            // 关闭 confirm 询问框
+            layer.close(index);
+        });
+    })
+})
+
+
+
+//获取用户信息
+function getuserinfo() {
+    $.ajax({
+        method: 'get',
+        url: '/my/userinfo',
+
+        success: function (res) {
+            //判断
+            if (res.status !== 0) {
+                return layui.layer.message
+            }
+            //渲染用户头像
+            renderpic(res.data)
+        },
+
+        
+    })
+
+}
+
+
+
+function renderpic(user) {
+    //获取用户名称
+    var name = user.nickname || user.username;
+    //渲染欢迎文本
+    $('#welcome1').html('欢迎' + name);
+    //判断有没有图片头像
+    if (user.user_pic !== null) {
+        //渲染图片头像
+        $('.layui-nav-img').attr('src', user.user_pic).show();
+        $('.text-avatar').hide()
+    } else {
+        //渲染文字头像
+        var first = name[0].toUpperCase();//将用户名称首字母大写
+        $('.text-avatar').html(first).show();
+        $('.layui-nav-img').hide()
+    }
+}
